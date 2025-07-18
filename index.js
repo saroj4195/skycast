ae97b23b6508eefff6582af54f6b132651bc01e0
saroj4195 <sarojkumarpatel12@gmail.com>
2025-07-18 11:09:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
+// Home route
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Welcome to Express!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+// About route
+app.get('/about', (req, res) => {
+  res.send('This is the about page.');
+});
+
+// JSON route
+app.get('/api/user', (req, res) => {
+  res.json({ name: "John", age: 25 });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
